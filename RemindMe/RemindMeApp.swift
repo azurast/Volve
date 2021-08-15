20d5b4970cfef1120cd37f31ed052460774b5a8c
Azura <azstima@gmail.com>
2021-08-15 13:55:11 +0700

    Initial Commit

---
+++ RemindMe/RemindMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindMeApp.swift
+//  RemindMe
+//
+//  Created by Azura Sakan Taufik on 15/08/21.
+//
+
+import SwiftUI
+
+@main
+struct RemindMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
