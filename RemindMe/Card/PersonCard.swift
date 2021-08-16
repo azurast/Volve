6032b1d80e89318eff2f9675366152958c4f4467
Azura <azstima@gmail.com>
2021-08-16 13:38:42 +0700

    enhance days left logic, add more avatars & randomized it

--- RemindMe/Card/PersonCard.swift
+++ RemindMe/Card/PersonCard.swift
@@ -22,40 +22,15 @@ func createFormatter() -> DateFormatter {
     return formatter
 }
 
-func getDaysLeftFromToday(from birthDate: Date, until currentDate: Date) -> Int {
-    /*
-        TODO
-            set year
-            if date & month udah lewat
-            year = thisyear+1
-            else
-            year = thisyear
-     */
-    let calendar = Calendar(identifier: .gregorian)
-    
-    let monthComp = calendar.dateComponents([.month], from: birthDate)
-    let dayComp = calendar.dateComponents([.day], from: birthDate)
-    
-    let birthdateComponents = DateComponents(month: monthComp.month, day: dayComp.day)
-    
-    if let nextBirthday = Calendar.current.nextDate(after: currentDate, matching: birthdateComponents, matchingPolicy: .strict),
-       let daysLeftUntilNextBirthday = Calendar.current.dateComponents([.day], from: currentDate, to: nextBirthday).day {
-        // TODO : Ceiling
-        print("days left \(daysLeftUntilNextBirthday)")
-        return daysLeftUntilNextBirthday
-    }
-    return 0
-}
-
 struct PersonCard: View {
     @State var person : PersonEntity
-    
     var formatter = createFormatter()
     var today = Date()
+    var x : Int = Int(arc4random_uniform(6))
     
     var body: some View {
         HStack(alignment: .center, spacing: 20) {
-            Image("av_1")
+            Image("av_\(x)")
                 .resizable()
                 .frame(width: 50, height: 50, alignment: .leading)
                 .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
@@ -76,7 +51,7 @@ struct PersonCard: View {
             }
             Spacer()
             VStack {
-                Text(String(getDaysLeftFromToday(from: person.birthday!, until: today))).foregroundColor(.accentColor).font(.callout).bold()
+                Text(String(person.daysLeft)).foregroundColor(.accentColor).font(.callout).bold()
                 Text("days left").foregroundColor(.secondary).font(.caption)
             }.padding(.trailing, 20)
         }
@@ -91,7 +66,9 @@ struct CategoryCard: View {
     @State var categoryName : String
     var body: some View {
         ZStack {
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            Button(action: {
+                print("clicked")
+            }, label: {
                 Text(categoryName).foregroundColor(.white).bold().font(.subheadline)
             }).frame(width: 100, height: 20,alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).padding(.all, 8)
         }.background(Color("AccentColor"))
--- RemindMe/ContentView.swift
+++ RemindMe/ContentView.swift
@@ -19,6 +19,7 @@ enum PersonCategory: String, CaseIterable, Identifiable {
 class CoreDataViewModel: ObservableObject {
     let container: NSPersistentContainer
     @Published var savedPeople: [PersonEntity] = []
+    var today = Date()
     
     init() {
         container = NSPersistentContainer(name: "RemindMeContainer")
@@ -34,8 +35,13 @@ class CoreDataViewModel: ObservableObject {
     
     func fetchPeople() {
         let request = NSFetchRequest<PersonEntity>(entityName: "PersonEntity")
+        let sort = NSSortDescriptor(keyPath: \PersonEntity.daysLeft, ascending: true)
+        request.sortDescriptors = [sort]
         do {
             savedPeople = try container.viewContext.fetch(request)
+            for people in savedPeople {
+                people.setValue(getDaysLeftFromToday(from: people.birthday ?? today, until: today), forKey: "daysLeft")
+            }
         } catch let error {
             print("ERROR FETCHING CORE DATA REQUEST. \(error)")
         }
@@ -60,6 +66,24 @@ class CoreDataViewModel: ObservableObject {
         }
     }
     
+    func getDaysLeftFromToday(from birthDate: Date, until currentDate: Date) -> Int32 {
+        let calendar = Calendar(identifier: .gregorian)
+        
+        let monthComp = calendar.dateComponents([.month], from: birthDate)
+        let dayComp = calendar.dateComponents([.day], from: birthDate)
+        
+        let birthdateComponents = DateComponents(month: monthComp.month, day: dayComp.day)
+       
+        if let nextBirthday = Calendar.current.nextDate(after: currentDate, matching: birthdateComponents, matchingPolicy: .strict),
+           let daysLeftUntilNextBirthday = Calendar.current.dateComponents([.day], from: currentDate, to: nextBirthday).day {
+            // TODO : Ceiling
+            print("days left \(daysLeftUntilNextBirthday)")
+            return Int32(daysLeftUntilNextBirthday)
+        }
+        return 0
+        
+    }
+    
 }
 
 struct ModalView: View {
@@ -71,41 +95,33 @@ struct ModalView: View {
     @State private var birthday = Date()
     
     var body: some View {
+        
         NavigationView {
-//            VStack(alignment: .center) {
-//                Image("av_1")
-//                    .resizable()
-//                    .frame(width: 100, height: 100)
-//                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
-//                    .aspectRatio(contentMode: .fit)
-//            }
-            VStack(alignment: .leading, spacing: 8) {
-                Text("First Name")
-                    .bold()
+            Form {
+                VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 16) {
+                    HStack {
+                        Spacer()
+                        Image("av_1")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .padding(EdgeInsets(top: 16, leading: 0, bottom: 0, trailing: 0))
+                            .aspectRatio(contentMode: .fit)
+                        Spacer()
+                    }
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Image(systemName: "camera.fill")
+                            .foregroundColor(.accentColor)
+                    }.padding(EdgeInsets(top: 0, leading: 0, bottom: 8, trailing: 0))
+                }
                 TextField("First Name", text: $firstName)
-                Text("Last Name")
-                    .bold()
                 TextField("Last Name", text: $lastName)
-                Text("Category")
-                    .bold()
-                HStack {
+                Picker("Category", selection: $category) {
                     ForEach(PersonCategory.allCases) {
-                        category in CategoryCard(categoryName: category.rawValue.capitalized)
+                        category in Text(category.rawValue.capitalized)
                     }
                 }
-//                Picker("Category", selection: $category) {
-//                    ForEach(PersonCategory.allCases) {
-//                        category in Text(category.rawValue.capitalized)
-//                    }
-//                }
-                Text("Birthday")
-                    .bold()
                 DatePicker("Birthday", selection: $birthday, in: ...Date(), displayedComponents: .date)
-                    .labelsHidden()
-                    .datePickerStyle(DefaultDatePickerStyle())
-                
             }
-            .padding(20)
             .navigationTitle("Add a Birthday")
             .navigationBarTitleDisplayMode(.inline)
             .navigationBarItems(
@@ -129,22 +145,32 @@ struct ContentView: View {
     @ObservedObject var vm = CoreDataViewModel()
     @State private var showModal = false
     @State var firstName : String
+    var today = Date()
     
     var body: some View {
         NavigationView {
             ScrollView {
                 LazyVStack(spacing: 20) {
-                    ForEach(vm.savedPeople) { entity in
+                    ForEach(vm.savedPeople.sorted(by: { $0.daysLeft < $1.daysLeft })) { entity in
                         PersonCard(person: entity)
                     }
                 }
             }
             .navigationTitle("Birthdays")
-            .navigationBarItems(trailing: Button(action: {
-                showModal.toggle()
-            }, label: {
-                Image.init(systemName: "plus")
-            }))
+            .toolbar{
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        showModal.toggle()
+                    }, label: {
+                        Image.init(systemName: "gearshape")
+                    })
+                    Button(action: {
+                        showModal.toggle()
+                    }, label: {
+                        Image.init(systemName: "plus")
+                    })
+                }
+            }
             .sheet(isPresented: $showModal, content: {
                 ModalView(vm: vm)
             })
